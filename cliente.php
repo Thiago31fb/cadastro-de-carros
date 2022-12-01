2a6f95eebbfd47770ee8c7b71dc0895f9fc21f10
Thiago Barcelo <thiago33fb@gmail.com>
2022-12-01 19:44:08 -0300

    first commit

---
+++ cliente.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="cliente.css">
+    <title>Cadastro</title>
+</head>
+<body>
+
+    <div class="containers">
+        <form action="" method="post">
+            <h1>Cadastro de Clientes</h1>
+            <p>Nome:</p>  <input type="text" name="nome">
+            <p>Data de nascimento:</p> <input type="date" name="data" id="data">
+            <input type="button" value="Cadastrar">
+        </form>
+    </div>
+
+    <div class="containers">
+        <form action="" method="post">
+            <h1>Cadastro Carro</h1>
+            <p>Placa:</p> <input type="text">
+            <p>Cor:</p> <input type="text">
+            <input type="button" value="Cadastrar">
+        </form>
+    </div>
+
+    <div class="containers">
+        <form action="" method="post">
+            <h1>Modelo</h1>
+            <p>Modelo:</p> <input type="text">
+            <input type="button" value="Cadastrar">
+        </form>
+    </div>
+
+
+    <div class="containers">
+        <form action="post">
+            <h1>Patio</h1>
+            <p>Capacidade:</p> <input type="number">
+            <p>Rua:</p> <input type="text">
+            <p>Bairro:</p> <input type="text">
+            <p>Numero:</p> <input type="number">
+            <input type="button" value="Cadastrar">
+        </form>
+    </div>
+</body>
+</html>
